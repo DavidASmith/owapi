185f3352067179eeb3fbf3e4df75f36bf5d3320f
David Smith <david.alex.smith@gmail.com>
2020-05-12 16:48:49 +0100

    Proof of concept getting data from API.

---
+++ owapi/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+import requests
+from pandas.io.json import json_normalize
+
+
+def get_api_key():
+    return(os.environ.get('OPENWEATHERAPIKEY'))
+
+def set_api_key(api_key):
+    os.environ['OPENWEATHERAPIKEY'] = api_key
+    
+def get_current_and_forecast(lat, lon):
+    
+    url = "https://api.openweathermap.org/data/2.5/onecall"
+    
+    params = {
+        "lat": lat, 
+        "lon": lon,
+        "units": "metric",
+        "appid": get_api_key()
+        }
+    
+    response = requests.get(url, 
+                            params = params)
+    
+    data = response.json()
+      
+    return(data)
+
+
+def get_current_obs(lat, lon):
+    current_forecast_response = get_current_and_forecast(lat, lon)
+
+    current = json_normalize(current_forecast_response['current'])
+    
+    return(current)
---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='owapi',
+      version='0.1',
+      description='Weather',
+      url='http://github.com',
+      author='David Smith',
+      author_email='david.alex.smith@gmail.com',
+      license='MIT',
+      packages=['owapi'],
+      zip_safe=False)
